dc5d77fea4118c30a28e9d912ef498cff813a9d5
Marco <patrickstar1281@yahoo.com>
2023-10-25 13:15:31 -0500

    flappy setup

---
+++ funtions.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+#include "functions.hpp"
+using namespace std;
+
+FlappyBird::FlappyBird() {
+    
+}
---
+++ funtions.hpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#pragma once
+#include <SFML/Graphics.hpp>
+#inlcude <memory>
+using namespace std;
+
+class FlappyBird {
+    shared_ptr<sf::RenderWindow> window;
+    float gravity, frame, space;
+    int count;
+
+    sf::Texture bg, flappy, pipe;
+    shared_ptr<sf::Sprite> background, bird, pipeBottom, pipeTop;
+    vector<sf::Sprite> pipes;
+
+    protected:
+        void events();
+        void draw();
+
+    public:
+        FlappyBird();
+        void run();
+};
