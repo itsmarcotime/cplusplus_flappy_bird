ead0376a52406b05dcfb3fa4bdbacac46412139a
Marco <patrickstar1281@yahoo.com>
2023-10-31 21:13:34 -0500

    added function to function.cpp

--- funtions.cpp
+++ funtions.cpp
@@ -3,5 +3,10 @@
 using namespace std;
 
 FlappyBird::FlappyBird() {
-    
+    window = make_shared<sf::RenderWindow>(
+        sf::VideoMode(1000, 600),
+        "Flappy Bird (remix)",
+        sf::Style::TitleBar | sf::Style::CLose
+        
+    );
 }
